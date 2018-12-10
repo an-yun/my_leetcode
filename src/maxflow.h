a8fe36983c06c10283e1be01da606d36b08dc10e
Fangyuan Zuo <mg1733104@smail.nju.edu.cn>
2018-12-10 22:10:36 +0800

    添加最大网络流代码文件

---
+++ src/maxflow.h
@@ -0,0 +1,17 @@
+/*
+ * This file is for maxflow problem using Ford-Fulkerson algorithm, Edmonds-Karp algorithm, Dinic's algorithm, Dinic's algorithm with dynamic trees data structure, General push_relabel,  FIFO push_relabel and preflow_push.
+ * Time complexity:
+ *      Ford-Fulkerson algorithm : m |f_max|
+ *      Edmonds-Karp algorithm : nm^2
+ *      Dinic's algorithm : n^2m
+ *      Dinic's algorithm with dynamic trees data structure : nm log n
+ *      General push_relabel : n^2m
+ *      FIFO push_relabel : n^3
+ *      preflow_push algorithm : n^2m^{1/2}
+ *for n nodes, m edges and f_max maxflow value
+ *
+ * author: zuo
+ * date  : 2018年 12月 10日 星期一 22:06:42 CST
+ */
+
+
