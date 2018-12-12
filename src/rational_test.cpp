b6cd887f2d371be0d04e6d660327027cd1799b27
Fangyuan Zuo <mg1733104@smail.nju.edu.cn>
2018-12-12 15:06:04 +0800

    提交昨天更改

--- src/rational_test.cpp
+++ src/rational_test.cpp
@@ -1,17 +1,8 @@
 /*
- * This file is for maxflow problem using Ford-Fulkerson algorithm, Edmonds-Karp algorithm, Dinic's algorithm, Dinic's algorithm with dynamic trees data structure, General push_relabel,  FIFO push_relabel and preflow_push.
- * Time complexity:
- *      Ford-Fulkerson algorithm : m |f_max|
- *      Edmonds-Karp algorithm : nm^2
- *      Dinic's algorithm : n^2m
- *      Dinic's algorithm with dynamic trees data structure : nm log n
- *      General push_relabel : n^2m
- *      FIFO push_relabel : n^3
- *      preflow_push algorithm : n^2m^{1/2}
- *for n nodes, m edges and f_max maxflow value
+ * This file is for test rational class
  *
  * author: zuo
- * date  : 2018年 12月 10日 星期一 22:06:42 CST
+ * date  : 2018年 12月 11日 星期二 22:46:43 CST
  */
 #include "boost/rational.hpp"
 #include "zuo/rational.hpp"
