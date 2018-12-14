e97e6fe2e4bce9130196ce141917628043542dbc
Fangyuan Zuo <mg1733104@smail.nju.edu.cn>
2018-12-14 17:28:50 +0800

    去掉多余的std

--- src/maxflow_test.cpp
+++ src/maxflow_test.cpp
@@ -1,17 +1,19 @@
 #include "zuo/maxflow.hpp"
 #include <iostream>
 
+using namespace std;
+
 void double_2_rational_test()
 {
-    std::vector<double> test_nums =
+    vector<double> test_nums =
         {0.12,1.5,0.003,-0.125,0,1.23,-1.246};
     for(auto num:test_nums)
     {
         auto r = zuo::double2ratonal(num);
-        std::cout << num << "->" << r<<", ";
+        cout << num << " -> " << r<<", ";
         if(std::abs(boost::rational_cast<double>(r) - num)> zuo::EPSILON)
-            std::cout << "test failed for :" << num << "->" << r ;
-        std::cout << std::endl;
+            cout << "test failed for :" << num << "->" << r ;
+        cout << endl;
     }
 }
 
