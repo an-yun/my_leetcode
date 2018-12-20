af34e77b73f9224ccbca97488ea31034956ad5c0
Fangyuan Zuo <mg1733104@smail.nju.edu.cn>
2018-12-20 12:55:46 +0800

    完成31题

--- src/leetcode31.cpp
+++ src/leetcode31.cpp
@@ -8,8 +8,28 @@ using namespace std;
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        // 从最后一位开始修改
-        next_permutation(nums.begin(), nums.end());
+        //标准库  next_permutation(nums.begin(), nums.end());
+        /*
+         参考 http://􀋋sherlei.blogspot.com/2012/12/leetcode-next-permutation.html
+         */
+        // 特殊情况
+        if(nums.size()<= 1)
+            return;
+        
+        // 找到第一个降序的位置
+        auto b = nums.rbegin();
+        for (auto  e = nums.rend(); b+1 != e; b++)
+            if(*(b+1) < *b )
+                break;
+        // 如果找到了
+        if(++b != nums.rend())
+        {
+            auto l = upper_bound(nums.rbegin(), b,*b);
+            swap(*b, *l);
+            reverse(nums.rbegin(), b);
+        }
+        else
+            reverse(nums.begin(),nums.end());
     }
 };
 
@@ -17,7 +37,7 @@ public:
 int main()
 {
     Solution s;
-    vector<int> nums{1, 2, 3, 3, 1};
+    vector<int> nums{1,1,5};
     print_vector(nums);
     s.nextPermutation(nums);
     cout << "Next permutation:" << endl;
