9f86a42709f7da5e60d2702deaf5f771bee8e503
Fangyuan Zuo <mg1733104@smail.nju.edu.cn>
2018-12-25 13:42:47 +0800

    修改代码编排

--- test/zhd_problem1/answer.cpp
+++ test/zhd_problem1/answer.cpp
@@ -1,106 +1,67 @@
-// C++ program for optimal allocation of pages
-#include <bits/stdc++.h>
+#include <algorithm>
+#include <cstdio>
+#include <numeric>
+
 using namespace std;
 
-// Utility function to check if current minimum value
-// is feasible or not.
-bool isPossible(int arr[], int n, int m, int curr_min)
+int T, N, A[250], M;
+
+bool check_validity(int max_pages)
 {
-    int studentsRequired = 1;
-    int curr_sum = 0;
-
-    // iterate over all books
-    for (int i = 0; i < n; i++)
+    int min_students = 1, current_pages = 0;
+    for (int i = 0; i < N; i++)
     {
-        // check if current number of pages are greater
-        // than curr_min that means we will get the result
-        // after mid no. of pages
-        if (arr[i] > curr_min)
+        if (A[i] > max_pages)
             return false;
-
-        // count how many students are required
-        // to distribute curr_min pages
-        if (curr_sum + arr[i] > curr_min)
+        if (current_pages + A[i] > max_pages)
         {
-            // increment student count
-            studentsRequired++;
-
-            // update curr_sum
-            curr_sum = arr[i];
-
-            // if students required becomes greater
-            // than given no. of students,return false
-            if (studentsRequired > m)
+            min_students++;
+            current_pages = A[i];
+            if (min_students > M)
                 return false;
         }
-
-        // else update curr_sum
         else
-            curr_sum += arr[i];
+            current_pages += A[i];
     }
     return true;
 }
 
-// function to find minimum pages
-int findPages(int arr[], int n, int m)
+int min_max_pages()
 {
-    long long sum = 0;
-
-    // return -1 if no. of books is less than
-    // no. of students
-    if (n < m)
+    // for special cases
+    if (M > N)
         return -1;
+    if (M == N)
+        return *max_element(A, A + N);
 
-    // Count total number of pages
-    for (int i = 0; i < n; i++)
-        sum += arr[i];
-
-    // initialize start as 0 pages and end as
-    // total pages
-    int start = 0, end = sum;
-    int result = INT_MAX;
-
-    // traverse until start <= end
+    //binary search
+    int end = accumulate(A, A + N, 0), start = end / M;
+    int min_pages = __INT_MAX__;
     while (start <= end)
     {
-        // check if it is possible to distribute
-        // books by using mid as current minimum
         int mid = (start + end) / 2;
-        if (isPossible(arr, n, m, mid))
+        if (check_validity(mid))
         {
-            // if yes then find the minimum distribution
-            result = min(result, mid);
-
-            // as we are finding minimum and books
-            // are sorted so reduce end = mid -1
-            // that means
+            min_pages = mid;
             end = mid - 1;
         }
-
         else
-            // if not possible means pages should be
-            // increased so update start = mid + 1
             start = mid + 1;
     }
-
-    // at-last return minimum no. of  pages
-    return result;
+    return min_pages;
 }
 
-// Drivers code
 int main()
 {
-    int T, N, A[250], M;
-    while (scanf("%d", &T) == 1)
+    scanf("%d", &T);
+
+    for (int i = 0; i < T; i++)
     {
-        for (int i = 0; i < T; i++)
-        {
-            scanf("%d", &N);
-            for (int n = 0; n < N; n++)
-                scanf("%d", A + n);
-            scanf("%d", &M);
-            printf("%d\n", findPages(A, N, M));
-        }
+        scanf("%d", &N);
+        for (int n = 0; n < N; n++)
+            scanf("%d", A + n);
+        scanf("%d", &M);
+        printf("%d\n", min_max_pages());
     }
     return 0;
 }
