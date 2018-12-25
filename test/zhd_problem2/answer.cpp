713040231c834c44469b734e18f90dc587eaef4d
Fangyuan Zuo <mg1733104@smail.nju.edu.cn>
2018-12-25 15:30:36 +0800

    修改测试代码

--- test/zhd_problem2/answer.cpp
+++ test/zhd_problem2/answer.cpp
@@ -1,67 +1,29 @@
 #include <algorithm>
 #include <cstdio>
+#include <limits>
 #include <numeric>
+#include <set>
 
 using namespace std;
-
-int T, N, A[250], M;
-
-bool check_validity(int max_pages)
-{
-    int min_students = 1, current_pages = 0;
-    for (int i = 0; i < N; i++)
-    {
-        if (A[i] > max_pages)
-            return false;
-        if (current_pages + A[i] > max_pages)
-        {
-            min_students++;
-            current_pages = A[i];
-            if (min_students > M)
-                return false;
-        }
-        else
-            current_pages += A[i];
-    }
-    return true;
-}
-
-int min_max_pages()
-{
-    // for special cases
-    if (M > N)
-        return -1;
-    if (M == N)
-        return *max_element(A, A + N);
-
-    //binary search
-    int end = accumulate(A, A + N, 0), start = end / M;
-    int min_pages = __INT_MAX__;
-    while (start <= end)
-    {
-        int mid = (start + end) / 2;
-        if (check_validity(mid))
-        {
-            min_pages = mid;
-            end = mid - 1;
-        }
-        else
-            start = mid + 1;
-    }
-    return min_pages;
-}
+typedef unsigned long long ULL;
 
 int main()
 {
+    int T, N;
+    multiset<ULL> nums;
     scanf("%d", &T);
-
-    for (int i = 0; i < T; i++)
+    for (int t = 0; t < T; t++)
     {
         scanf("%d", &N);
-        for (int n = 0; n < N; n++)
-            scanf("%d", A + n);
-        scanf("%d", &M);
-        printf("%d\n", min_max_pages());
+        ULL num,inversion_count = 0;
+        for (int n = 0; n < N;n++)
+        {
+            
+            scanf("%llu", &num);
+            inversion_count += distance(nums.upper_bound(num), nums.end());
+            nums.insert(num);
+        }
+        printf("%llu\n", inversion_count);
     }
     return 0;
 }
--- test/zhd_problem2/generate_testcases.cpp
+++ test/zhd_problem2/generate_testcases.cpp
@@ -4,17 +4,15 @@
 #include <iostream>
 #include <random>
 #include <string>
+#include <numeric>
 
 using namespace std;
+typedef unsigned long long ULL;
 
-const int T_MIN = 1, T_MAX = 70;
-const int N_MIN = 1, N_MAX = 50;
-const int A_MIN = 1, A_MAX = 250;
-const int M_MIN = 1, M_MAX = 50;
+const int N_MIN = 1, N_MAX = 100000000;
+const ULL C_MIN = 1, C_MAX = 10000000000000000000ULL;
 
-void gernerate_one_testcase(fstream &f, int edge_flag);
-inline int compute_edge_flag(int T_flag, int N_flag, int A_flag, int M_flag);
-inline void get_edge_flag(int edge_flag, int &T_flag, int &N_flag, int &A_flag, int &M_flag);
+void gernerate_one_testcase(fstream &f, int N_flag, int C_flag);
 
 int main()
 {
@@ -26,15 +24,12 @@ int main()
     {
         /*
         边缘条件标志
-        T       0:随机 1:最小 2:最大
         N       0:随机 1:最小 2:最大
-        A       0:随机 1:最小 2:升序 3:降序 4:最大
-        M       0:最小 1:<=N 2:>N 3:最大
+        C       0:随机 1:最小 2:升序 3:降序 4:最大
         */
 
-        int T_flag = 0, N_flag = 0, A_flag = 0, M_flag = 1;
-        int edge_flag = compute_edge_flag(T_flag, N_flag, A_flag, M_flag);
-        gernerate_one_testcase(f, edge_flag);
+        int  N_flag = 0, C_flag = 0;
+        gernerate_one_testcase(f, N_flag, C_flag);
         /** 总共10个case, 包括示例,其余九个的设置为
          * 依次设置的结果是
          * 1 0 0 1
@@ -53,61 +48,38 @@ int main()
     return 0;
 }
 
-void gernerate_one_testcase(fstream &f, int edge_flag)
+void gernerate_one_testcase(fstream &f, int N_flag, int C_flag)
 {
-    int T_flag, N_flag, A_flag, M_flag;
-    get_edge_flag(edge_flag, T_flag, N_flag, A_flag, M_flag);
-    cout << "edge flag: " << T_flag << " " << N_flag << " " << A_flag << " " << M_flag << endl;
+
+    cout << "edge flag: " << N_flag << " " << C_flag << endl;
 
     // set random
     default_random_engine gen(time(NULL));
-
-    uniform_int_distribution dis_T(T_MIN, T_MAX);
     uniform_int_distribution dis_N(N_MIN, N_MAX);
-    uniform_int_distribution dis_A(A_MIN, A_MAX);
-    uniform_int_distribution dis_M(M_MIN, M_MAX);
+    uniform_int_distribution<ULL> dis_C(C_MIN, C_MAX);
 
-    int T, N, A[250], M;
 
-    // set T
-    switch (T_flag)
-    {
-    case 0:
-        T = dis_T(gen);
-        break;
-    case 1:
-        T = T_MIN;
-        break;
-    default:
-        T = T_MAX;
-        break;
-    }
-    //save T
-    f << T << endl;
+    //9 testcases
+    f << 9 << endl;
 
-    for (int t = 0; t < T; t++)
+    for (int t = 0; t < 9; t++)
     {
         // set N
         switch (N_flag)
         {
         case 0:
-            N = dis_N(gen);
-            if (N == 1)
-                N = 2;
+
             break;
         case 1:
-            N = N_MIN;
+
             break;
         default:
-            N = N_MAX;
+
             break;
         }
-        // set A
-        for (int n = 0; n < N; n++)
-        {
-            A[n] = dis_A(gen);
-        }
-        switch (A_flag)
+        // set C
+
+        switch (C_flag)
         {
         case 1:
             fill(A, A + N, A_MIN);
@@ -124,42 +96,6 @@ void gernerate_one_testcase(fstream &f, int edge_flag)
         default:
             break;
         }
-        // set M
-        switch (M_flag)
-        {
-        case 0:
-            M = M_MIN;
-            break;
-        case 1:
-            M = (dis_M(gen) - M_MIN) * (N - N_MIN) / (M_MAX - M_MIN) + N_MIN;
-            break;
-        case 2:
-            M = min((dis_M(gen) - M_MIN) * (N_MAX - N) / (M_MAX - M_MIN) + N + 1, M_MAX);
-            break;
-        default:
-            M = M_MAX;
-            break;
-        }
-
-        // save testcase
-
-        f << N << endl;
-        for (int n = 0; n < N; n++)
-            f << A[n] << " ";
-        f << endl;
-        f << M << endl;
     }
     f << endl;
-}
-
-inline int compute_edge_flag(int T_flag, int N_flag, int A_flag, int M_flag)
-{
-    return (((T_flag * 3) + N_flag) * 5 + A_flag) * 4 + M_flag;
-}
-inline void get_edge_flag(int edge_flag, int &T_flag, int &N_flag, int &A_flag, int &M_flag)
-{
-    M_flag = edge_flag % 4;
-    A_flag = (edge_flag /= 4) % 5;
-    N_flag = (edge_flag /= 5) % 3;
-    T_flag = edge_flag / 3;
 }
