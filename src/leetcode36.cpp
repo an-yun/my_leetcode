1fecc16aeaeed4192ee2993003b73684e6a9432f
Fangyuan Zuo <mg1733104@smail.nju.edu.cn>
2019-01-04 14:18:18 +0800

    初步解决36题

--- src/leetcode36.cpp
+++ src/leetcode36.cpp
@@ -1,27 +1,46 @@
+#include <algorithm>
 #include <iostream>
 #include <vector>
-#include <queue>
+#include <set>
 #include "zuo/ioutils.hpp"
 
 using namespace std;
 
 class Solution {
-private:
-    vector<vector<char>> m_board;
-    
-    bool check_board()
-    {
-
-        
-        return true;
-    }
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
-        m_board = board;    
-        return check_board();        
+        bool repeat_flag[9];
+        // check row
+        for(int i=0;i<9;i++)
+        {
+            fill(repeat_flag, repeat_flag+9,false);
+            for(auto c:board[i])
+            {
+                if(c == '.') continue;
+                if( repeat_flag[c-'1'])
+                    return false;
+                repeat_flag[c-'1']=true;
+            }
+        }
+        // check column
+        for(int j=0;j<9;j++)
+        {
+            fill(repeat_flag, repeat_flag+9,false);
+            for(int i=0;i<9;i++)
+            {
+                auto c = board[i][j];
+                if(c == '.') continue;
+                if( repeat_flag[c-'1'])
+                    return false;
+                repeat_flag[c-'1'] = true;
+            }
+        }
+                
+        return true;
     }
 };
 int main()
 {
+    
     return 0;
 }
---
+++ src/leetcode37.cpp
@@ -0,0 +1,28 @@
+
+#include <iostream>
+#include <vector>
+#include <queue>
+#include "zuo/ioutils.hpp"
+
+using namespace std;
+
+class Solution {
+private:
+    vector<vector<char>> m_board;
+    
+    bool check_board()
+    {
+
+        
+        return true;
+    }
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+        m_board = board;    
+        return check_board();        
+    }
+};
+int main()
+{
+    return 0;
+}
