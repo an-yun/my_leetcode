a4fcea3c1c43459a5d7c18328eff42cf27c9dd75
Fangyuan Zuo <mg1733104@smail.nju.edu.cn>
2019-01-07 14:01:54 +0800

    添加38题代码

---
+++ src/leetcode38.cpp
@@ -0,0 +1,22 @@
+#include "zuo/ioutils.hpp"
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Solution
+{
+  public:
+    string countAndSay(int n)
+    {
+    }
+};
+
+int main()
+{
+
+    Solution s;
+    println(s.countAndSay(4));
+    return 0;
+}
