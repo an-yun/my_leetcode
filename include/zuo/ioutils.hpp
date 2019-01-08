81845142b9f3497a0d9bcfee0249fd8c0ce1d761
Fangyuan Zuo <mg1733104@smail.nju.edu.cn>
2019-01-08 15:32:05 +0800

    修改打印代码,添加39题代码

--- include/zuo/ioutils.hpp
+++ include/zuo/ioutils.hpp
@@ -12,15 +12,18 @@ inline void print(const T &t)
 template <typename T>
 inline void print(const std::vector<T> &t)
 {
-    std::cout << '[';
+    print('[');
     size_t t_size = t.size();
     if (t_size > 0)
     {
         for (size_t i = 1; i < t_size; i++)
-            std::cout << t[i - 1] << ", ";
-        std::cout << t[t_size - 1];
+        {
+            print(t[i - 1]);
+            print(", ");
+        }
+        print(t[t_size - 1]) ;
     }
-    std::cout << ']';
+    print(']');
 }
 // 泛化的打印函数
 template <typename T, typename... Args>
---
+++ src/leetcode39.cpp
@@ -0,0 +1,22 @@
+#include "zuo/ioutils.hpp"
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        
+    }
+};
+
+int main()
+{
+    vector<int> nums{2, 3, 6, 7};
+    int target = 7;
+    Solution s;
+    println(s.combinationSum(nums,target));
+    return 0;
+}
