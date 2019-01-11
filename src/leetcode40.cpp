b902a985ed9efddf4cee07f5f45e96cbb3801932
Fangyuan Zuo <mg1733104@smail.nju.edu.cn>
2019-01-11 13:54:41 +0800

    解决40题

--- src/leetcode40.cpp
+++ src/leetcode40.cpp
@@ -3,6 +3,7 @@
 #include <map>
 #include <string>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
@@ -11,19 +12,25 @@ class Solution
   public:
     vector<vector<int>> combinationSum2(vector<int> &candidates, int target)
     {
+        map<int,int> count;
+        for(auto num:candidates) count[num]++;
         map<int, vector<vector<int>>> c;
         /*
         * The recurrence is 
-        * C[i][t] = \cup_{k=0}^{t/n_i} \{s\cup \{n_i\}*k |s\in C[i-1][t-k*n_i]\}
+        * C[i][t] = \cup_{k=0}^{count_i} \{s\cup \{n_i\}*k |s\in C[i-1][t-k*n_i]\}
         * 
         * where C[i][t] is the set consisting of numbers from 1st to i-th, the sum of which is t. $\{n_i\}*k$ means a set with k n_i, for example $\{3\}*2$ means $\{3,3\}$.
          */
-        for (auto num : candidates)
+        for (auto num_count_pair : count)
         {
+            int num = num_count_pair.first;
             auto pre_c = c;
-            c[k * num].push_back(vector<int>(k, num));
-            for (auto &k_v : pre_c)
+            auto num_count = count[num];
+            for (int k = 1; k <= num_count; k++)
             {
+                c[k * num].push_back(vector<int>(k, num));
+                for (auto &k_v : pre_c)
+                {
                     int sum = k_v.first + num * k;
                     if (sum <= target)
                     {
@@ -36,6 +43,7 @@ class Solution
                         }
                     }
                 }
+            }
         }
         return c[target];
     }
@@ -43,8 +51,8 @@ class Solution
 
 int main()
 {
-    vector<int> nums{2, 3, 5};
-    int target = 8;
+    vector<int> nums{2,5,2,1,2};
+    int target = 5;
     Solution s;
     println(s.combinationSum2(nums, target));
     return 0;
