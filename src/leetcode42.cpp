6773df09c9b4f7e702a3b844191570a7f67555a0
Fangyuan Zuo <mg1733104@smail.nju.edu.cn>
2019-01-12 14:27:52 +0800

    添加42题题目和代码

---
+++ src/leetcode42.cpp
@@ -0,0 +1,19 @@
+#include <algorithm>
+#include <vector>
+#include "zuo/ioutils.hpp"
+
+using namespace std;
+
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        
+    }
+};
+
+int main()
+{
+    Solution s;
+    vector<int> height{0,1,0,2,1,0,1,3,2,1,2,1};
+    println(s.trap(height));
+}
