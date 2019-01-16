c8337213a60eaf89959bc3449bb29c04c41f3aa2
Fangyuan Zuo <mg1733104@smail.nju.edu.cn>
2019-01-16 14:08:33 +0800

    添加42题代码

---
+++ src/leetcode43.cpp
@@ -0,0 +1,24 @@
+#include "zuo/ioutils.hpp"
+#include <algorithm>
+#include <list>
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+    string multiply(string num1, string num2) 
+    {
+        if(num1 == "0" || num2 == "0" )
+            return "0";
+        string result;
+        // to do
+        return result;
+    }
+};
+
+int main()
+{
+    Solution s;
+    println(s.multiply("123","456"));
+}
