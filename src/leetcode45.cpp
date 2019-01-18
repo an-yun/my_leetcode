db2c80e1d3633105ee411cc4d20d9deaf245a4a7
Fangyuan Zuo <mg1733104@smail.nju.edu.cn>
2019-01-18 14:20:05 +0800

    添加45题代码

---
+++ src/leetcode45.cpp
@@ -0,0 +1,26 @@
+#include "zuo/ioutils.hpp"
+#include <algorithm>
+#include <list>
+#include <map>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+  public:
+    int jump(vector<int>& nums)
+    {
+        
+        return 0; 
+    }
+
+};
+
+int main()
+{
+    Solution s;
+    vector<int> nums{2,3,1,1,4};
+    println(s.jump(nums));
+    return 0;
+}
