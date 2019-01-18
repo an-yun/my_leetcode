9c9c5d1d05c71058fcd3891e876cb4c39ffe7bde
Fangyuan Zuo <mg1733104@smail.nju.edu.cn>
2019-01-18 14:50:21 +0800

    完成45题

--- src/leetcode45.cpp
+++ src/leetcode45.cpp
@@ -8,13 +8,33 @@
 using namespace std;
 
 class Solution {
-  public:
+public:
     int jump(vector<int>& nums)
     {
-        
-        return 0; 
+        int len = nums.size();
+        if(len < 2) return 0;
+        //breadth first search
+        vector<int> dis(len, -1);
+        list<int> visit_queue;
+        dis[0] = 0;
+        visit_queue.push_back(0);
+        while(dis[len-1] == -1)
+        {
+            auto node = visit_queue.front();
+            visit_queue.pop_front();
+            auto num = nums[node];
+            for(int j=1;j<=num && j+node < len;j++)
+            {
+                auto next_node = j+node;
+                if(dis[next_node] == -1)
+                {
+                    visit_queue.push_back(next_node);
+                    dis[next_node] = dis[node] + 1; 
+                }            
+            }
+        }
+        return dis[len-1]; 
     }
-
 };
 
 int main()
