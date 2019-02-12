69bbfe4f51597daa2c21d85c00edce0ff2c14e57
Fangyuan Zuo <mg1733104@smail.nju.edu.cn>
2019-02-12 11:33:33 +0800

    完成48题

--- src/leetcode48.cpp
+++ src/leetcode48.cpp
@@ -8,7 +8,11 @@ using namespace std;
 class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
-        
+        reverse(matrix.begin(), matrix.end());
+        size_t n = matrix.size();
+        for (size_t i = 0; i < n;i++)
+            for (size_t j = 0; j < i;j++)
+                swap(matrix[i][j], matrix[j][i]);
     }
 };
 
