0d7982857e67325149e7d92d9ab0a0d4df86e1d0
Fangyuan Zuo <mg1733104@smail.nju.edu.cn>
2019-03-06 14:49:00 +0800

    完成55题

--- src/leetcode55.cpp
+++ src/leetcode55.cpp
@@ -8,16 +8,25 @@ using namespace std;
 
 class Solution
 {
-    public:
+  public:
     bool canJump(vector<int> &nums)
     {
-        return false;
+        int max_jump = 0, len = nums.size();
+        for (int i = 0; i < len;i++)
+        {
+            max_jump = max(max_jump, i + nums[i]);
+            if(max_jump >= len -1)
+                return true;
+            else if (max_jump == i)
+                return false;
+        }
+        return true;
     }
 };
 
 int main()
 {
-    vector<int> nums{2, 3, 1, 1, 4};
+    vector<int> nums{3,2,1,0,4};
     Solution s;
     println(s.canJump(nums));
     return 0;
