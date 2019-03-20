9e98d685574777c1510d9eebc30c304478258ad9
Fangyuan Zuo <mg1733104@smail.nju.edu.cn>
2019-03-20 15:41:09 +0800

    完成174题

---
+++ src/leetcode174.cpp
@@ -0,0 +1,93 @@
+#include "zuo/ioutils.hpp"
+#include <algorithm>
+#include <cmath>
+#include <initializer_list>
+#include <map>
+#include <numeric>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Solution
+{
+  public:
+    int calculateMinimumHP(vector<vector<int>> &dungeon)
+    {
+        size_t m = dungeon.size(), n = dungeon[0].size();
+        vector<vector<int>> health = dungeon;
+        int upper_health =1+ ( health[0][0] < 0 ? -health[0][0] : 0);
+        for (size_t j = 1; j < n; ++j)
+        {
+            health[0][j] += health[0][j - 1];
+            upper_health = max(1-health[0][j], upper_health);
+        }
+
+        for (size_t i = 1; i < m; ++i)
+        {
+            health[i][0] += health[i - 1][0];
+            upper_health = max(1-health[i][0], upper_health);
+        }
+
+        for (size_t i = 1; i < m; ++i)
+        {
+            for (size_t j = 1; j < n; ++j)
+            {
+                health[i][j] += max(health[i - 1][j], health[i][j - 1]);
+                upper_health = max(1-health[i][j], upper_health);
+            }
+        }
+        //binary search
+        int l = 1, r = upper_health;
+        while (l < r)
+        {
+            int mid = (l + r) / 2;
+            if ((health[0][0] = dungeon[0][0] + mid) <= 0)
+            {
+                l = mid + 1;
+                continue;
+            }
+
+            for (size_t j = 1; j < n; ++j)
+                health[0][j] = health[0][j - 1] <=0? -1:health[0][j - 1] + dungeon[0][j];
+
+            for (size_t i = 1; i < m; ++i)
+                health[i][0] = health[i - 1][0]<=0? -1:health[i - 1][0]+ dungeon[i][0];
+
+            for (size_t i = 1; i < m; ++i)
+                for (size_t j = 1; j < n; ++j)
+                {
+                    health[i][j] = dungeon[i][j];
+                    if (health[i - 1][j] <= 0)
+                    {
+                        if (health[i][j - 1] <= 0)
+                            health[i][j] = -1;
+                        else
+                            health[i][j] += health[i][j - 1];
+                    }
+                    else if (health[i][j - 1] <= 0)
+                        health[i][j] += health[i - 1][j];
+                    else
+                        health[i][j] += max(health[i][j - 1], health[i - 1][j]);
+                }
+
+            // traceback
+            if (health[m-1][n-1] <= 0)
+                l = mid + 1;
+            else
+                r = mid;
+        }
+        return r;
+    }
+};
+
+int main()
+{
+    Solution s;
+    vector<vector<int>> dungeon{
+        
+{-2,-3,3},{-5,-10,1},{10,30,-5}
+        };
+    println(s.calculateMinimumHP(dungeon));
+    return 0;
+}
