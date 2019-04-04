2f0780cf2a00596b3ae938432e203437b5aadcf1
Fangyuan Zuo <mg1733104@smail.nju.edu.cn>
2019-04-04 15:42:52 +0800

    添加79题更优解

--- src/leetcode79.cpp
+++ src/leetcode79.cpp
@@ -13,23 +13,21 @@ using namespace std;
 class Solution
 {
   public:
-    bool dfs(vector<vector<char>> &board, string &word, size_t start, int x, int y,int m, int n,vector<vector<bool>> &visited)
+    bool dfs(vector<vector<char>> &board, string &word, size_t start, int x, int y, int m, int n)
     {
         if (start >= word.size()) // match case
             return true;
-        if (x < 0 || x >= m || y < 0 || y >= n )// edge case
+        if (x < 0 || x >= m || y < 0 || y >= n) // edge case
             return false;
-        if(visited[x][y] || word[start] != board[x][y])// for match case
+        char temp = board[x][y];        // for save memory
+        if (word[start] != board[x][y]) // for match case
             return false;
         else
-            visited[x][y] = true; // Using board  can save memory here, for example char temp = borad[x][y];borad[x][y] = '\0';
-        if (dfs(board, word, start + 1, x - 1, y, m, n, visited) || dfs(board, word, start + 1, x + 1, y, m, n, visited) || dfs(board, word, start + 1, x, y - 1, m, n, visited) || dfs(board, word, start + 1, x, y + 1, m, n, visited))
-            return true; //  Corresponding to line 25 ,add one line above :borad[x][y] = temp;
-        else
-        {
-            visited[x][y] = false;
-            return false;
-        }
+            board[x][y] = '\0';// Using board  can save memory here
+        bool match_word = dfs(board, word, start + 1, x - 1, y, m, n) || dfs(board, word, start + 1, x + 1, y, m, n) ||
+                          dfs(board, word, start + 1, x, y - 1, m, n) || dfs(board, word, start + 1, x, y + 1, m, n);
+        board[x][y] = temp;// restore board[x][y]
+        return match_word;
     }
     bool exist(vector<vector<char>> &board, string word)
     {
@@ -39,15 +37,14 @@ class Solution
         if (m == 0)
             return false;
         int n = board[0].size();
-        if(n == 0)
+        if (n == 0)
             return false;
-        // dfs search 
-        vector<vector<bool>> visited(m, vector<bool>(n, false));
+        // dfs search
         for (int i = 0; i < m; ++i)
         {
             for (int j = 0; j < n; ++j)
             {
-                if (dfs(board, word, 0, i, j, m, n, visited)) //try every position
+                if (dfs(board, word, 0, i, j, m, n)) //try every position
                     return true;
             }
         }
@@ -62,6 +59,6 @@ int main()
         {'A', 'B', 'C', 'E'},
         {'S', 'F', 'C', 'S'},
         {'A', 'D', 'E', 'E'}};
-    println(s.exist(board, "ABCCED"));
+    println(s.exist(board, "ABCB"));
     return 0;
 }
