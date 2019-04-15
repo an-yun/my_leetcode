eb78c627df5b8be36d12c64a3f50691fa44f3c3d
Fangyuan Zuo <mg1733104@smail.nju.edu.cn>
2019-04-15 11:18:38 +0800

    添加85题更优解

--- src/leetcode85.cpp
+++ src/leetcode85.cpp
@@ -13,7 +13,7 @@ using namespace std;
 class Solution
 {
   public:
-  int largestRectangleArea(vector<int> &heights)
+    int largestRectangleArea(vector<int> heights)
     {
         int heights_size = heights.size();
         if (heights_size == 0)
@@ -22,8 +22,8 @@ class Solution
         list<int> h_stack{0};
         for (int i = 1; i < heights_size; ++i)
         {
-            int top = h_stack.back(),current_top = top;
-            if(heights[i] == heights[current_top])
+            int top = h_stack.back(), current_top = top;
+            if (heights[i] == heights[current_top])
                 continue;
             int last_i = i;
             while (!h_stack.empty() && heights[i] < heights[current_top = h_stack.back()])
@@ -38,7 +38,7 @@ class Solution
         while (!h_stack.empty())
         {
             int current_top = h_stack.back();
-            max_area = max(max_area, heights[current_top] * (heights_size - current_top ));
+            max_area = max(max_area, heights[current_top] * (heights_size - current_top));
             h_stack.pop_back();
         }
         return max_area;
@@ -47,26 +47,26 @@ class Solution
     int maximalRectangle(vector<vector<char>> &matrix)
     {
         size_t m = matrix.size();
-        if(m == 0)
+        if (m == 0)
             return 0;
         size_t n = matrix[0].size();
-        if(n == 0)
+        if (n == 0)
             return 0;
-        vector<vector<int>> max_area(m, vector<int>(n, 0));
-        // tranform to largestRectangleArea problem
-        for (size_t j = 0; j < n; ++j)
-        {
-            max_area[0][j] = matrix[0][j] - '0';
-            for (size_t i = 1; i < m; ++i)
-            {
-                if(matrix[i][j] == '1')
-                    max_area[i][j] = max_area[i-1][j] + 1;
-            }
-        }
-        // solve largestRectangleArea problem for echo row
+        vector<int> row_heights(n, 0);
         int result = 0;
-        for(auto &row:max_area)
-            result = max(result, largestRectangleArea(row));
+        // tranform to largestRectangleArea problem
+        for (size_t i = 0; i < m; ++i)
+        {
+            for (size_t j = 0; j < n; ++j)
+            {
+                if (matrix[i][j] == '1')
+                    row_heights[j] ++;
+                else
+                    row_heights[j] = 0;
+            }
+            // solve largestRectangleArea problem for echo row
+            result = max(result, largestRectangleArea(row_heights));
+        }
         return result;
     }
 };
@@ -79,12 +79,10 @@ int main()
         {'1', '0', '1', '1', '1'},
         {'1', '1', '1', '1', '1'},
         {'1', '0', '0', '1', '0'}},
-    matrix1
-    {
-        {'1','1','0'},
-        {'1','1','1'},
-        {'0','1','0'}
-    };
+        matrix1{
+            {'1', '1', '0'},
+            {'1', '1', '1'},
+            {'0', '1', '0'}};
     println(s.maximalRectangle(matrix1));
     return 0;
 }
