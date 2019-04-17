c3431651ecd940e355505a79b43f91f592fd9f48
Fangyuan Zuo <mg1733104@smail.nju.edu.cn>
2019-04-17 09:55:05 +0800

    添加82题代码

---
+++ src/leetcode82.cpp
@@ -0,0 +1,23 @@
+#include "zuo/ioutils.hpp"
+#include <algorithm>
+
+using namespace std;
+
+struct ListNode{
+    int val;
+    ListNode *next;
+    ListNode(int x):val(x),next(nullptr){}
+};
+
+class Solution{
+    ListNode* deleteDuplicates(ListNode* head)
+    {
+        return head;
+    }
+};
+
+int main()
+{
+
+}
+
