2be8bb4fd2834d32dc92725994ea1184182fdac5
Fangyuan Zuo <mg1733104@smail.nju.edu.cn>
2019-05-05 09:51:56 +0800

    完成91题

--- src/leetcode91.cpp
+++ src/leetcode91.cpp
@@ -13,25 +13,36 @@ using namespace std;
 class Solution {
 public:
     int numDecodings(string s) {
-        int size = s.size();
-        if(size == 0) return 1;
-        switch(s[0])
+        int len = s.size();
+        if(len == 0) return 1;
+        int cur = s[--len]!='0', last = 1,temp;
+        while(--len >= 0)
         {
-            case '0':
-                return 0;
-            case '1':
-                return numDecodings(s.substr(1)) + (size > 1? numDecodings(s.substr(2)):0);
-            case '2':
-                if(s[1] <= '6') return numDecodings(s.substr(1)) + (size > 1? numDecodings(s.substr(2)):0);
-            default:
-                return numDecodings(s.substr(1));
+            temp = cur;
+            switch(s[len])
+            {
+                case '0':
+                    cur = 0;
+                    break;
+                case '1':
+                    cur += last;
+                    break;
+                case '2':
+                    if( s[len+1] < '7') cur += last;
+                    break;
+                default:
+                    break;
+
+            }
+            last = temp;
         }
+        return cur;
     }
 };
 
 int main()
 {
     Solution s;
-    println(s.numDecodings("162"));
+    println(s.numDecodings("226"));
     return 0;
 }
