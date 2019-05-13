5ff980df0a0517cef2cf7f2cbfbe4cecb2b19baf
Fangyuan Zuo <mg1733104@smail.nju.edu.cn>
2019-05-13 09:23:21 +0800

    添加更多测试用例

--- test/treenode_test.cpp
+++ test/treenode_test.cpp
@@ -2,7 +2,7 @@
 
 int main()
 {
-    auto tree = make_tree({1,null,2,3});
+    auto tree = make_tree({1,32,2,3,null,34,null,4,5,null,null,6,null,7,8,9,null,null,10});
     println(tree);
     free_tree(tree);
     return 0;
