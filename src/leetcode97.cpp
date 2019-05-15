9ee451e41c83ace6f3baddc69a6f368c4eda061c
Fangyuan Zuo <mg1733104@smail.nju.edu.cn>
2019-05-15 20:44:48 +0800

    完成97题

--- src/leetcode97.cpp
+++ src/leetcode97.cpp
@@ -1,31 +1,40 @@
 #include "zuo/ioutils.hpp"
+#include <algorithm>
+#include <array>
 #include <string>
-
+#include <list>
+#include <limits>
 using namespace std;
 
 class Solution {
 public:
     bool isInterleave(string s1, string s2, string s3) {
-        int m = s1.size(), n = s2.size(), l = s3.size();
+        size_t m = s1.size(), n = s2.size(), l = s3.size();
         if(m+n != l)
             return false;
         if(l == 0)
             return true;
-        vector<bool> is_inter(l, false);
-        while(l--)
+        else if (m == 0)
+            return s2 == s3;
+        else if (n == 0)
+            return s1 == s3;
+
+        vector<bool> is_inter(m+1,true);
+        for (size_t j = 1; j <= m; ++j)
+                is_inter[j] = (is_inter[j - 1] && s1[j - 1] == s3[j - 1]);
+        for (size_t i = 1; i <= n; ++i)
         {
-            if(m && s1[--m])
-            {
-                
-            }
+            is_inter[0] = is_inter[0] && s2[i - 1] == s3[i - 1];
+            for (size_t j = 1; j <= m; ++j)
+                is_inter[j] = (is_inter[j] && s2[i - 1 ] == s3[i + j - 1] ) || (is_inter[j - 1] && s1[j - 1] == s3[i + j - 1]);
         }
-        return is_inter[0];
+        return is_inter.back();
     }
 };
 
 int main()
 {
     Solution s;
-    println(s.isInterleave("aabcc", "dbbca", "aadbbcbcac"));
+    println(s.isInterleave("aacaac","aacaaeaac","aacaaeaaeaacaac"));
     return 0;
 }
