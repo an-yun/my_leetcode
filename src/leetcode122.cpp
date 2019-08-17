894218022a7b7999002f32c30a5093a51720c006
Fangyuan Zuo <mg1733104@smail.nju.edu.cn>
2019-08-17 13:00:05 +0800

    完成122题

--- src/leetcode122.cpp
+++ src/leetcode122.cpp
@@ -9,15 +9,14 @@ public:
     int maxProfit(vector<int>& prices) {
         if(prices.size() <= 1)
             return 0;
-        int sell = prices.back(), profit = 0;
-        // for (auto b = ++prices.rbegin(), e = prices.rend(); b != e;++b)
-        // {
-        //     if(*b >= sell)
-        //         sell = *b;
-        //     else
-        //         profit = max(profit, sell - *b);
+        int buy = prices.front(), profit = 0;
+        for (auto b = ++prices.begin(), e = prices.end(); b != e;++b)
+        {
+            if(*b > buy)
+                profit += *b - buy;
+            buy = *b;    
 
-        // }
+        }
         return profit;
     }
 };
@@ -25,7 +24,7 @@ public:
 int main()
 {
     Solution s;
-    vector<int> prices{7, 1, 5, 3, 6, 4};
+    vector<int> prices{1,2,3,4,5};
     println(s.maxProfit(prices));
     return 0;
 }
